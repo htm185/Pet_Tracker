64d4d3983da0afe8773b15f874e4ac3a885597f6
Hoàng Thảo My <hoangthaomy185@gmail.com>
2026-04-24 17:10:49 +0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,14 +3,14 @@ import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getDatabase, onValue } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBVMaHOEF_MV8G3nDGY5ghwBpeoFDJ3_xM",
+    apiKey: "",
     authDomain: "kinhdovido.firebaseapp.com",
     databaseURL: "https://kinhdovido-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "kinhdovido",
     storageBucket: "kinhdovido.firebasestorage.app",
-    messagingSenderId: "56568392591",
-    appId: "1:56568392591:web:836fffc6279f060de539fa",
-    measurementId: "G-P6H9XFR3LQ"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 
 };
 
